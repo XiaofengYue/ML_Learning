7167fff856edac6a45a9373c52c801d8ab871d7b
YXF <736659711@qq.com>
2021-09-07 11:19:00 +0800

    新的总结

--- BOOKS/2-马尔可夫决策过程/utils.py
+++ BOOKS/2-马尔可夫决策过程/utils.py
@@ -11,9 +11,30 @@ def str_key(*args):
 def set_dict(target_dict, value, *args):
     target_dict[str_key(*args)] = value
 
-def set_prob(P, s, a, s1, p = 1.0):
+def set_prob(P,s,a,s1,p=1.0):# 设置概率字典
     set_dict(P, p, s, a, s1)
 
-def get_prob(P, s, a, s1):
-    P.get(str_key(s, a, s1), 0)
+def get_prob(P,s,a,s1):# 获取概率值 
+    return P.get(str_key(s,a,s1), 0)
 
+def set_reward(R, s, a, r):
+    set_dict(R, r, s, a)
+
+def get_reward(R, s, a):
+    return R.get(str_key(s, a), 0)
+
+def display_dict(target_dict):
+    for key in target_dict.keys():
+        print("{}:  {:.2f}".format(key, target_dict[key]))
+
+def set_value(V, s, v):
+    set_dict(V, v, s)
+
+def get_value(V, s):
+    return V.get(str_key(s), 0)
+
+def set_pi(Pi, s, a, p=0.5):
+    set_dict(Pi, p, s, a)
+
+def get_pi(Pi, s, a):
+    return Pi.get(str_key(s, a), 0)
